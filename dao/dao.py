bb128017ebab9d2f47562a408c1da4a5a08f3937
douglasfcsoares <douglasfcsoares@gmail.com>
2025-05-04 23:17:42 -0300

    DAO Category

---
+++ dao/dao.py
@@ -0,0 +1,24 @@
+from model.model import *
+
+class DaoCategory:
+    @classmethod
+    def save(cls, category):
+        with open('dao/category.txt', 'a') as arq:
+            arq.writelines(category)
+            arq.writelines('\n')
+
+    @classmethod
+    def read(cls):
+        with open('category.txt', 'r') as arq:
+            cls.category = arq.readlines()
+
+        cls.category = list(map(lambda x: x.replace('\n', ''), cls.category))
+        cat = []
+        for i in cls.category:
+            cat.append(Category(i))
+            return cat
+
+# DaoCategory.save('Frutas')
+# DaoCategory.save('Verduras')
+# DaoCategory.save('Legumes')
+DaoCategory.read()
